da8c6a668f07051007fd776930e87bc9782bb4d5
AlexBuck <underflux@googlemail.com>
2024-10-22 02:06:37 +0100

    fixed errors from vercel

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import Header from "./components/Header";
 export default function Homepage() {
   return (
     <>
-      <h1>Get your dose of street art/grafitti here!</h1>
+      <h1>Get your dose of street art/graffiti here!</h1>
       <p>
-        Welcome to my blog where I will be repping THE very best in grafitti and
+        Welcome to my blog where I will be repping THE very best in graffiti and
         street art around the world. I appreciate that art is subjective, but
-        trust me, I've got an eye for quality. I view street art and grafitti in
+        trust me, I have an eye for quality. I view street art and grafitti in
         particular as bedfellows with my other passion that is music, and more
         specifically drum and bass. They both come from the street and has deep
         roots culturally with what is sometimes known as the 'concrete jungle'.
-        Being born in London and living in areas where life wasn't always easy,
+        Being born in London and living in areas where life can be challenging,
         the urban chaos became a backdrop for youth to express themselves
         artistically. The music of drum and bass also came from these very same
         communities - forgotten communities in some ways. This artistic
